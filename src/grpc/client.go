90fddb144e53255a50ada2170051948c7ac19c83
tinchomorilla <mmorilla@fi.uba.ar>
2025-07-02 18:50:52 -0300

    implement gRPC client for DatasetService with connection and GetBatch method

---
+++ src/grpc/client.go
@@ -0,0 +1,27 @@
+package grpc
+
+import (
+    "context"
+    "google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "github.com/mlops-eval/data-dispatcher-service/src/pb"
+)
+
+
+type Client struct {
+    conn   *grpc.ClientConn
+    client pb.DatasetServiceClient
+}
+
+func NewClient(address string) (*Client, error) {
+    conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+    if err != nil {
+        return nil, err
+    }
+    client := pb.NewDatasetServiceClient(conn)
+    return &Client{conn: conn, client: client}, nil
+}
+
+func (c *Client) GetBatch(ctx context.Context, req *pb.GetBatchRequest) (*pb.DataBatch, error) {
+    return c.client.GetBatch(ctx, req)
+}
