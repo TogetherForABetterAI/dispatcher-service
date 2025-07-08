0078f7262f35e39f3deb11d3d1cd356178ecbe1e
tinchomorilla <mmorilla@fi.uba.ar>
2025-07-08 18:15:00 -0300

    remove proto-gen service and related Dockerfile and script

--- src/scripts/generate_proto.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Delete the old generated code
-sudo rm -rf src/pb/
-mkdir -p src/pb
-
-# Run the proto-gen container to regenerate the code
-docker compose run --rm proto-gen 
