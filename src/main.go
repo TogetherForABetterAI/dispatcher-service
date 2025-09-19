a9ef3f7b8ff8a7a0e6280c9a49bc46310b6ca1dc
tinchomorilla <mmorilla@fi.uba.ar>
2025-09-18 21:43:44 -0300

    refactor: replace gRPC server with RabbitMQ consumer and update logging

--- src/main.go
+++ src/main.go
@@ -1,68 +1,75 @@
 package main
 
 import (
+	"log"
+	"log/slog"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 
-	"github.com/joho/godotenv"
-	"github.com/mlops-eval/data-dispatcher-service/src/grpc"
+	"github.com/mlops-eval/data-dispatcher-service/src/config"
 	"github.com/mlops-eval/data-dispatcher-service/src/processor"
-	"github.com/sirupsen/logrus"
+	"github.com/mlops-eval/data-dispatcher-service/src/rabbitmq"
 )
 
 func main() {
-	// Load environment variables
-	if err := godotenv.Load(); err != nil {
-		// It's okay if .env file doesn't exist
+	config := loadConfig()
+	setupLogging()
+	consumer := createConsumer(config)
+	startServiceWithGracefulShutdown(consumer, config)
+}
+
+func loadConfig() *config.GlobalConfig {
+	config, err := config.NewConfig()
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
 	}
+	return config
+}
 
-	// Setup logging
-	logger := logrus.New()
-	logger.SetFormatter(&logrus.JSONFormatter{})
+func setupLogging() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}))
+	slog.SetDefault(logger)
+}
 
-	// Set log level from environment
-	if logLevel := os.Getenv("LOG_LEVEL"); logLevel != "" {
-		if level, err := logrus.ParseLevel(logLevel); err == nil {
-			logger.SetLevel(level)
-		}
-	}
-
-	// Get port from environment or use default
-	port := 50058
-	if portStr := os.Getenv("GRPC_PORT"); portStr != "" {
-		if parsed, err := strconv.Atoi(portStr); err == nil {
-			port = parsed
-		}
-	}
-
-	logger.WithFields(logrus.Fields{
-		"service": "data-dispatcher-service",
-		"version": "1.0.0",
-		"port":    port,
-	}).Info("Starting data dispatcher service")
-
+func createConsumer(config *config.GlobalConfig) *rabbitmq.Consumer {
 	// Create client processor
 	clientProcessor := processor.NewClientDataProcessor()
 
-	// Create gRPC server
-	grpcServer := grpc.NewDataDispatcherServer(clientProcessor)
+	// Create RabbitMQ consumer for new client connection notifications
+	consumer, err := rabbitmq.NewConsumer(config.MiddlewareConfig, clientProcessor)
+	if err != nil {
+		log.Fatalf("Failed to create RabbitMQ consumer: %v", err)
+	}
 
-	// Start server in a goroutine
+	return consumer
+}
+
+func startServiceWithGracefulShutdown(consumer *rabbitmq.Consumer, config *config.GlobalConfig) {
+	// Channel to listen for interrupt signal
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start consumer in a goroutine
 	go func() {
-		logger.WithField("port", port).Info("Starting gRPC server")
-		if err := grpcServer.Start(port); err != nil {
-			logger.WithError(err).Fatal("Failed to start gRPC server")
+		slog.Info("Starting service",
+			"service", config.ServiceConfig.Name,
+			"version", config.ServiceConfig.Version)
+
+		if err := consumer.Start(); err != nil {
+			log.Fatalf("Failed to start RabbitMQ consumer: %v", err)
 		}
+
+		slog.Info("RabbitMQ consumer started successfully")
 	}()
 
-	// Wait for shutdown signal
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
-	<-stop
+	// Wait for interrupt signal
+	<-quit
+	slog.Info("Shutting down service...")
 
-	logger.Info("Received shutdown signal, initiating graceful shutdown")
-	grpcServer.Stop()
-	logger.Info("Server stopped successfully")
+	// Attempt graceful shutdown
+	consumer.Stop()
+	slog.Info("Service exited gracefully")
 }
