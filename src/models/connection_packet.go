d74b84e719fa767236d8d6ce0b4dbf2f3492a15c
tinchomorilla <mmorilla@fi.uba.ar>
2025-10-10 17:12:34 -0300

    rename file

--- src/models/connection_packet.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// ConnectNotification represents the message structure from the auth gateway
-type ConnectNotification struct {
-	ClientId      string `json:"client_id"`
-	InputsFormat  string `json:"inputs_format"`
-	OutputsFormat string `json:"outputs_format"`
-}
